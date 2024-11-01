8632763c206fce7d144425472a2f89b0debff8e1
George Wood <git@gwood.dev>
2024-11-01 19:46:23 +0000

    Implemented the following methods for the DepartureBoard struct: - add_departure() - remove_departure()
    Implemented the following utility functions for the DepartureBoard struct:
    - print_departures()
    - sort_by_eta()

--- src/departure_board.rs
+++ src/departure_board.rs
@@ -5,7 +5,27 @@ pub struct DepartureBoard {
 }
 
 impl DepartureBoard {
-    pub fn new(departures: Vec<Departure>) -> Self {
-        Self { departures }
+    pub fn new() -> Self {
+        Self { departures: Vec::new() }
+    }
+
+    pub fn add_departure(&mut self, departure: Departure) {
+        self.departures.push(departure);
+        self.sort_by_eta()
+    }
+
+    pub fn remove_departure(&mut self, index: usize) {
+        self.departures.remove(index);
+        self.sort_by_eta()
+    }
+
+    pub fn print_departures(&self) {
+        for departure in self.departures.iter() {
+            println!("{}\n", departure.summarise_to_string())
+        }
+    }
+
+    fn sort_by_eta(&mut self) {
+        self.departures.sort_by(|a, b| a.eta().timestamp().cmp(&b.eta().timestamp()))
     }
 }
